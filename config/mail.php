d31159463fbc74ca00431cb26bac57bee09dc61d
Aleksandar Savanovic <djaca85@gmail.com>
2016-08-15 13:32:33 +0200

    Update 'from' field

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'example@gmail.com', 'name' => 'Example'],
 
     /*
     |--------------------------------------------------------------------------
