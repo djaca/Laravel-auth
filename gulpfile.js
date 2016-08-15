4ccdf49c668f68f16877ee2d350ab93c32343322
Aleksandar Savanovic <djaca85@gmail.com>
2016-08-15 14:24:44 +0200

    Update assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,14 @@
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
 
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
+gulp.task('fonts', function() {
+    return gulp.src('node_modules/font-awesome/fonts/*')
+        .pipe(gulp.dest('public/build/fonts'))
+})
 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass('app.scss')
+        .webpack('app.js');
+
+    mix.version(['css/app.css', 'js/app.js'])
 });
